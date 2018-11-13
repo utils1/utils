4b8d375164c63e476bedd9244f6149fbe11c0d3d
utils1 <sobhanallah1003@yandex.com>
2018-11-13 22:20:33 +0330

    resource

---
+++ Csharp/resource.cs
@@ -0,0 +1,6 @@
+/*add file as resource. ex: a html file that you want read and send to server.
+*
+*/
+
+//In the properties dialog of your project open up the "Resources" tab and then add your file. You'll then be able to get the file using:
+//[ProjectNamespace].Properties.Resources.[MyFile]
